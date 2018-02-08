0f22f8a938de915295ae91e3dda5c8e5289c63fd
andrewyee12 <ayee@villanova.edu>
2018-02-08 17:24:52 +0100

    Scientist movement

--- GameJam2/Assets/Scripts/Player.cs
+++ GameJam2/Assets/Scripts/Player.cs
@@ -5,6 +5,8 @@ using System.Collections;
 
 public class Player : MonoBehaviour
 {
+  [Tooltip("How fast is the spaceship moving left to right")]
+    public float speed;
 
   public enum PlayerStatus
 	{
@@ -22,6 +24,14 @@ public class Player : MonoBehaviour
   SpriteRenderer[] sr;
   PlayerStatus status;
   Coroutine hurtRoutine;
+
+  void Update()
+  {
+    // move the ship left and right, depending on the horizontal input
+    transform.position += Vector3.right * Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+    transform.position += Vector3.up * Input.GetAxis("Vertical") * speed * Time.deltaTime;
+
+  }
 
   void Awake ()
 	{
