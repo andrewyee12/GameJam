55dec8c927da5f3ed1c29eb0d214919c43122d68
John Sica <jsica12@gmail.com>
2018-02-09 17:31:10 +0100

    Countdown timer

---
+++ GameJam2/Assets/Scripts/Timer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+	float timeLeft = 120.0f;
+	void Update(){
+		timeLeft -= Time.deltaTime;
+	}
+	void OnGUI(){
+		if (timeLeft >= 0) {
+			string formatTime = string.Format ("{0:00}", timeLeft);
+			GUI.Label (new Rect (10, 10, 250, 100), formatTime);
+		} else {
+			GUI.Label (new Rect (10, 10, 250, 100), "Game Over");
+		}
+
+	}
+}
+
